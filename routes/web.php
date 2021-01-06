25b0e9b50566f26fab2aa18fee4364317d20e7a1
charfeddine-naoufel <charfeddinenaoufel@gmail.com>
2021-01-06 21:08:54 +0100

    authentication jetstream+livewire

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,11 @@
 <?php
 
+use App\Http\Livewire\Admin\AdminDashboardComponent;
+use App\Http\Livewire\CartComponent;
+use App\Http\Livewire\CheckoutComponent;
 use App\Http\Livewire\HomeComponent;
+use App\Http\Livewire\ShopComponent;
+use App\Http\Livewire\User\UserDashboardComponent;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -19,3 +24,19 @@ use Illuminate\Support\Facades\Route;
 // });
 
 Route::get('/',HomeComponent::class);
+Route::get('/shop',ShopComponent::class);
+Route::get('/cart', CartComponent::class);
+Route::get('checkout',CheckoutComponent::class);
+// Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+//     return view('dashboard');
+// })->name('dashboard');
+
+// routes for User or customer
+Route::middleware(['auth:sanctum', 'verified'])->group(function () {
+Route::get('/user/dashboard',UserDashboardComponent::class)->name('user.dashboard');
+});
+// routes for Admin
+Route::middleware(['auth:sanctum', 'verified','authadmin'])->group(function () {
+    Route::get('/admin/dashboard',AdminDashboardComponent::class)->name('admin.dashboard');
+
+});
